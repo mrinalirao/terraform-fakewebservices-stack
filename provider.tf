2e806b4a20e75feb54bc1dd233f2438b5fcad2a6
Mrinali Rao <mrinali.rao@gmail.com>
2023-09-19 09:23:41 +1000

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -2,3 +2,12 @@ provider "fakewebservices" {
   hostname = var.fws_hostname
   token    = var.fake_token
 }
+
+terraform {
+  required_providers {
+    fakewebservices = {
+      source  = "hashicorp/fakewebservices"
+      version = "0.2.1"
+    }
+  }
+}
