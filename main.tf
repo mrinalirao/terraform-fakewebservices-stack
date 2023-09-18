88747356bc5e3b2f20ca4f708ccb2118e74f67c2
Mrinali Rao <mrinali.rao@gmail.com>
2023-09-19 09:21:37 +1000

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,29 +1,10 @@
-resource "fakewebservices_vpc" "vpc" {
-  name       = upper("${var.stack_prefix}_VPC")
-  cidr_block = var.vpc_cidr_block
-}
-
-resource "fakewebservices_server" "server" {
-  count = var.server_count
-
-  name = upper("SRV_${var.stack_prefix}_${count.index + 1}")
-  type = var.server_type
-  vpc  = fakewebservices_vpc.vpc.name
-
-  lifecycle {
-    postcondition {
-      condition     = self.type == "t2.micro"
-      error_message = "The fakewebservices_server server type is invalid"
-    }
-  }
-}
-
-resource "fakewebservices_load_balancer" "load_balancer" {
-  name    = upper("${var.stack_prefix}_LB")
-  servers = fakewebservices_server.server[*].name
-}
-
-resource "fakewebservices_database" "database" {
-  name = upper("${var.stack_prefix}_DB")
-  size = var.database_size
+// The Main Module
+module "stack" {
+  source         = "aqitio/stack/fakewebservices"
+  version        = "0.0.8"
+  stack_prefix   = "ENV"
+  server_count   = var.server_count
+  server_type    = "t2.micro"
+  database_size  = 128
+  vpc_cidr_block = "0.0.0.0/1"
 }
