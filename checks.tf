aa42af75589403a0d4e10dd002533b7d90b89a80
Mrinali Rao <mrinali.rao@gmail.com>
2023-09-19 10:14:04 +1000

    remove checks.tf

--- checks.tf
+++
@@ -1,8 +0,0 @@
-check "check_fakewebservices_database_size" {
-  assert {
-    condition = fakewebservices_database.database.size != 128
-    error_message = format("All database resources should have a database size of '128'. Got %s instead.",
-      fakewebservices_database.database.size
-    )
-  }
-}
